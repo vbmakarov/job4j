918b7f6af07d97ae988dec7e7b45d87a8da09a2a
vbmakarov <vbmakarov.info@gmail.com>
2021-05-11 22:39:41 +0300

    first commit

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main (String[] args){
+
+        System.out.println("1x1=1");
+        System.out.println("1x2=2");
+        System.out.println("1x3=3");
+        System.out.println("1x4=4");
+        System.out.println("1x5=5");
+        System.out.println("1x6=6");
+        System.out.println("1x7=7");
+        System.out.println("1x8=8");
+        System.out.println("1x9=9");
+        System.out.println("1x9=9");
+    }
+}
